0097171ccf3282ae4eaef42c3441c95c8e4bfca2
Guillame Tran <guillaume1.tran@epitech.eu>
2025-04-12 17:27:18 +0200

    ADD theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
+import { ThemeProvider } from "@theme/ThemeContext"
 
+import { Hub } from "@modules/hub/Hub"
 function App() {
   return (
-    <div></div>
+    <ThemeProvider>
+      <Hub></Hub>
+    </ThemeProvider>
   )
 }
 
---
+++ src/modules/hub/Hub.tsx
@@ -0,0 +1,18 @@
+import { useTheme } from "@theme/ThemeContext"
+
+export const Hub = () => {
+    const theme = useTheme()
+    return (
+        <div
+            style={{
+                backgroundColor: theme.colors.background,
+                color: theme.colors.text,
+                fontFamily: theme.typography.fontFamily,
+                fontSize: theme.typography.fontSize,
+            }}
+        >
+            <h1>Hub</h1>
+            <p>Welcome to the Hub!</p>
+        </div>
+    )
+}
