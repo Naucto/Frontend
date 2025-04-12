977777e03b55e63f2d07a243aa5666bfa7d1dc70
Guillame Tran <guillaume1.tran@epitech.eu>
2025-04-12 17:34:42 +0200

    feat(app): added route in website

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
 import { ThemeProvider } from "@theme/ThemeContext"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Hub } from "@modules/hub/Hub"
+
 function App() {
   return (
     <ThemeProvider>
-      <Hub></Hub>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hub />} />
+          <Route path="/hub" element={<Hub />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
 
 export default App
