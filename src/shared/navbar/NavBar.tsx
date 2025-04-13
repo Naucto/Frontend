e06d92c4ec26a8a4d7199070eef27f8192f12d14
Guillame Tran <guillaume1.tran@epitech.eu>
2025-04-13 10:18:40 +0200

    cleanup(eslint format): format based on rules

--- src/shared/navbar/NavBar.tsx
+++ src/shared/navbar/NavBar.tsx
@@ -1,26 +1,26 @@
-import { theme } from '@theme/theme';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { theme } from "@theme/theme";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <Link to="/">MyApp</Link>
-            </div>
-            <ul className="navbar-links">
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/services">Services</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">MyApp</Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
--- src/theme/ThemeContext.tsx
+++ src/theme/ThemeContext.tsx
@@ -1,17 +1,17 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext } from "react";
 
 
 export const theme = {
-    colors: {
-        primary: "#E5D352",
-        secondary: "#537D8D",
-        background: "#303030",
-        text: "#FFFFFF",
-    },
-    typography: {
-        fontFamily: "'Roboto', sans-serif",
-        fontSize: 16,
-    },
+  colors: {
+    primary: "#E5D352",
+    secondary: "#537D8D",
+    background: "#303030",
+    text: "#FFFFFF",
+  },
+  typography: {
+    fontFamily: "'Roboto', sans-serif",
+    fontSize: 16,
+  },
 }
 
 type ThemeType = typeof theme;
@@ -19,11 +19,11 @@ type ThemeType = typeof theme;
 const ThemeContext = createContext<ThemeType>(theme);
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <ThemeContext.Provider value={theme} >
-            {children}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={theme} >
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export const useTheme = () => useContext(ThemeContext);
