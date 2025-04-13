f4fbce298ac7e94715d0fedb554e82df2ddd4c6a
Guillame Tran <guillaume1.tran@epitech.eu>
2025-04-13 11:08:20 +0200

    feat(user): added user

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
@@ -12,10 +12,22 @@ import { CodeEditor } from "@modules/editor/CodeEditor/CodeEditor"
 import { MapEditor } from "@modules/editor/MapEditor/MapEditor"
 import { SoundEditor } from "@modules/editor/SoundEditor/SoundEditor"
 import { SpriteEditor } from "@modules/editor/SpriteEditor/SpriteEditor"
+import { useUser } from "@modules/user/UserContext"
 
 
 function App() {
+  const { user, setUser } = useUser();
 
+  useEffect(() => {
+    setUser({
+      "name": "test",
+      "id": "test",
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log("User", user)
+  }, [user])
   const editorManager = new EditorManager();
   editorManager.addEditor(new CodeEditor());
   editorManager.addEditor(new MapEditor());
@@ -24,7 +36,6 @@ function App() {
 
   return (
     <ThemeProvider>
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Hub />} />
