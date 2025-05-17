25aee595b7225d15294f3772a27da8bb606a15a5
Guillame Tran <guillaume1.tran@epitech.eu>
2025-05-17 12:20:00 +0200

    [GAME CANVAS][FEAT] added game canvas

---
+++ src/shared/canvas/gameCanvas/KeyHandler.ts
@@ -0,0 +1,24 @@
+
+export class KeyHandler {
+  private keysPressed: Set<string>;
+
+  constructor() {
+    this.keysPressed = new Set();
+  }
+
+  public handleKeyDown = (event: KeyboardEvent): void => {
+    this.keysPressed.add(event.key);
+  };
+
+  public handleKeyUp = (event: KeyboardEvent): void => {
+    this.keysPressed.delete(event.key);
+  };
+
+  public isKeyPressed(key: string): boolean {
+    return this.keysPressed.has(key);
+  }
+
+  public clearKeys(): void {
+    this.keysPressed.clear();
+  }
+}
