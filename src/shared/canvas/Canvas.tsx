4c390528608396ebfe76d2e0d10dfcbe438785cf
Guillame Tran <guillaume1.tran@epitech.eu>
2025-05-17 12:20:00 +0200

    [GAME CANVAS][FEAT] added game canvas

--- src/shared/canvas/Canvas.tsx
+++ src/shared/canvas/Canvas.tsx
@@ -1,9 +1,9 @@
 import { SpriteRendererHandle, useSpriteRenderer } from "@shared/canvas/RendererHandle";
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
 import { SpriteSheet } from "src/types/SpriteSheetType";
 import styled from "styled-components";
 
-type CanvasProps = {
+export type CanvasProps = {
   spriteSheet: SpriteSheet;
   screenSize: {
     width: number;
@@ -13,17 +13,26 @@ type CanvasProps = {
   className?: string;
 };
 
-const Canvas = forwardRef<SpriteRendererHandle, CanvasProps>(({ screenSize, spriteSheet, palette, className }, ref) => {
+export type CanvasHandle = SpriteRendererHandle & {
+  getCanvas: () => HTMLCanvasElement | null;
+};
+
+const Canvas = forwardRef<CanvasHandle, CanvasProps>(({ screenSize, spriteSheet, palette, className }, ref) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const rendererHandle = useSpriteRenderer(canvasRef, spriteSheet, palette, screenSize);
 
-  useImperativeHandle(ref, () => rendererHandle, [rendererHandle]);
+  useImperativeHandle(ref, () => ({
+    ...rendererHandle,
+    getCanvas: () => canvasRef.current,
+  }), [rendererHandle]);
+
   return (
     <canvas
       ref={canvasRef}
       width={screenSize.width}
       height={screenSize.height}
       className={className}
+      tabIndex={0}
     />
   );
 });
@@ -31,7 +40,7 @@ const Canvas = forwardRef<SpriteRendererHandle, CanvasProps>(({ screenSize, spri
 const StyledCanvas = styled(Canvas)`
   image-rendering: pixelated;
   width: 100%;
-
+  outline: none;
 `;
 
 export default StyledCanvas;
---
+++ src/shared/canvas/gameCanvas/GameCanvas.tsx
@@ -0,0 +1,96 @@
+import StyledCanvas, { CanvasHandle, CanvasProps } from "@shared/canvas/Canvas";
+import { KeyHandler } from "@shared/canvas/gameCanvas/KeyHandler";
+import { SpriteRendererHandle } from "@shared/canvas/RendererHandle";
+import { EnvData, LuaEnvironmentManager } from "@shared/luaEnvManager/LuaEnvironmentManager";
+import { forwardRef, useCallback, useEffect, useRef } from "react";
+
+type GameCanvasProps = {
+  canvasProps: CanvasProps;
+  envData: EnvData;
+  setOutput: (output: string) => void
+  className?: string;
+};
+
+const GameCanvas = forwardRef<SpriteRendererHandle, GameCanvasProps>(({ canvasProps: { screenSize, spriteSheet, palette, className }, envData, setOutput }, ref) => {
+  const spriteRendererHandleRef = useRef<CanvasHandle | null>(null);
+  const luaEnvManagerRef = useRef<LuaEnvironmentManager | null>(null);
+  const animationFrameRef = useRef<number | null>(null);
+  const keyHandlerRef = useRef<KeyHandler>(new KeyHandler);
+  const loop = useCallback((): void => {
+    if (!spriteRendererHandleRef.current) {
+      return;
+    }
+    const luaEnvManager = luaEnvManagerRef.current;
+    luaEnvManager?.update();
+    luaEnvManager?.draw();
+    animationFrameRef.current = requestAnimationFrame(loop);
+    spriteRendererHandleRef.current?.draw();
+  }, [luaEnvManagerRef, spriteRendererHandleRef]);
+
+  // init lua env
+  useEffect(() => {
+    if (spriteRendererHandleRef.current == null || keyHandlerRef.current == null) {
+      return;
+    }
+    const rendererHandle = spriteRendererHandleRef.current;
+    const keyHandler = keyHandlerRef.current;
+    luaEnvManagerRef.current = new LuaEnvironmentManager({
+      envData,
+      rendererHandle,
+      keyHandler,
+      setOutput
+    });
+  }, [envData, setOutput]);
+
+  // init key handler
+  useEffect(() => {
+    const spriteRenderer = spriteRendererHandleRef.current;
+    if (!spriteRenderer) {
+      return;
+    }
+    const canvas = spriteRenderer.getCanvas();
+    if (!canvas) {
+      return;
+    }
+    canvas.addEventListener("keydown", keyHandlerRef.current?.handleKeyDown);
+    canvas.addEventListener("keyup", keyHandlerRef.current?.handleKeyUp);
+
+    return () => {
+      canvas.removeEventListener("keydown", keyHandlerRef.current?.handleKeyDown);
+      canvas.removeEventListener("keyup", keyHandlerRef.current?.handleKeyUp);
+    };
+  }, []);
+
+  // global init
+  useEffect(() => {
+    if (!spriteRendererHandleRef.current || !luaEnvManagerRef.current) {
+      return;
+    }
+    const luaEnvManager = luaEnvManagerRef.current;
+
+    spriteRendererHandleRef.current.clear(0);
+    luaEnvManager.runCode();
+
+    luaEnvManager.init();
+    animationFrameRef.current = requestAnimationFrame(loop);
+
+    return () => {
+      if (animationFrameRef.current) {
+        cancelAnimationFrame(animationFrameRef.current);
+        animationFrameRef.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <StyledCanvas
+      ref={spriteRendererHandleRef}
+      screenSize={screenSize}
+      spriteSheet={spriteSheet}
+      palette={palette}
+      className={className}
+    />
+  );
+});
+
+export default GameCanvas;
