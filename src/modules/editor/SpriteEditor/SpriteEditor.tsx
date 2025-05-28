aa97d930a9c01e0906ef525052e243802eeb6b66
emaurel <edgar.maurel@gmail.com>
2025-05-28 20:26:54 +0200

    [SPRITE EDITOR][FEAT] handle drawing

--- src/modules/editor/SpriteEditor/SpriteEditor.tsx
+++ src/modules/editor/SpriteEditor/SpriteEditor.tsx
@@ -22,6 +22,7 @@ export const SpriteEditor: React.FC<SpriteEditorProps> = ({ doc, provider }) => 
   const [isDragging, setIsDragging] = useState(false);
   const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
   const [isMouseOverCanvas, setIsMouseOverCanvas] = useState(false);
+  const [editableSpriteTable, setEditableSpriteTable] = useState(spriteTable);
   const drawCanvasRef = React.createRef<SpriteRendererHandle>();
   const canvasContainerRef = useRef<HTMLDivElement>(null);
 
@@ -59,10 +60,10 @@ export const SpriteEditor: React.FC<SpriteEditorProps> = ({ doc, provider }) => 
       drawCanvasRef.current.queueSpriteDraw(0, position.x, position.y, 16, 16);
       drawCanvasRef.current.draw();
     }
-  }, [drawCanvasRef, position]);
+  }, [editableSpriteTable, drawCanvasRef, position]);
 
   const canvasSpriteSheet: SpriteSheet = {
-    spriteSheet: spriteTable,
+    spriteSheet: editableSpriteTable,
     spriteSize: {
       width: 8,
       height: 8
@@ -79,14 +80,23 @@ export const SpriteEditor: React.FC<SpriteEditorProps> = ({ doc, provider }) => 
     height: Math.floor(8 * zoom)
   };
 
+
+
   useEffect(() => {
     if (doc && provider) {
     }
   }, [doc, provider]);
 
   const handleClick = (x: number, y: number) => {
-    console.log("handleClick", x, y);
-    return;
+    const spriteArray = editableSpriteTable.split('');
+
+    const index = y * canvasSpriteSheet.size.width + x;
+
+    spriteArray[index] = currentColor.toString(16);
+
+    const newSpriteTable = spriteArray.join('');
+
+    setEditableSpriteTable(newSpriteTable);
   };
 
   const changeColor = (index: number) => {
@@ -98,12 +108,12 @@ export const SpriteEditor: React.FC<SpriteEditorProps> = ({ doc, provider }) => 
     const power = 5;
     setZoom(prevZoom => {
       const newZoom = Math.max(1, prevZoom + delta * power);
-      console.log("newZoom", newZoom);
       return Math.round(newZoom * 10) / 10;
     });
   };
 
   const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isMouseOverCanvas) return;
     if (e.button !== 2) return;
     setIsDragging(true);
     setDragStart({ x: e.clientX, y: e.clientY });
@@ -162,9 +172,13 @@ export const SpriteEditor: React.FC<SpriteEditorProps> = ({ doc, provider }) => 
               onClick={(e: React.MouseEvent<HTMLCanvasElement>) => {
                 if (!isDragging) {
                   const rect = e.currentTarget.getBoundingClientRect();
-                  const x = Math.floor((e.clientX - rect.left) / zoom);
-                  const y = Math.floor((e.clientY - rect.top) / zoom);
-                  handleClick(x, y);
+                  const spriteX = Math.floor(((e.clientX - rect.left) / rect.width * zoom) - (Math.floor(position.x) / 8));
+                  const spriteY = Math.floor(((e.clientY - rect.top) / rect.height * zoom) - (Math.floor(position.y) / 8));
+                  const x = Math.floor((e.clientX - rect.left) / (rect.width / zoom) * 8 - Math.floor(position.x)) % 8;
+                  const y = Math.floor((e.clientY - rect.top) / (rect.width / zoom) * 8 - Math.floor(position.y)) % 8;
+                  const spriteIndex = x + spriteX * 8;
+                  const pixelIndex = y + spriteY * 8;
+                  handleClick(spriteIndex, pixelIndex);
                 }
               }}
             />
