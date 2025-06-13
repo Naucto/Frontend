a81d76968338b850e68bf49ba56ac0b055ec63d1
Guillame Tran <guillaume1.tran@epitech.eu>
2025-06-13 22:28:02 +0200

    [EDITOR][REFACTOR] : added editors and preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React, { useMemo, useRef, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Hub } from "@modules/hub/Hub";
-import { EditorManager, EditorManagerProvider } from "@modules/editor/EditorManager";
-
-import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
-import { CodeEditor } from "@modules/editor/CodeEditor/CodeEditor";
-import { MapEditor } from "@modules/editor/MapEditor/MapEditor";
-import { SoundEditor } from "@modules/editor/SoundEditor/SoundEditor";
-import { SpriteEditor } from "@modules/editor/SpriteEditor/SpriteEditor";
 import styled from "styled-components";
 import { useTheme } from "@theme/ThemeContext";
 import NavBar from "@shared/navbar/NavBar";
@@ -30,39 +23,24 @@ const Container = styled.div<{ theme: any }>`
 function App() {
   // temporary for example
 
-  const editorManagerRef = useRef(new EditorManager());
-
-  const editorManager = useMemo(() => {
-    const manager = editorManagerRef.current;
-    manager.addEditor(CodeEditor, { title: "Code", icon: "code" });
-    manager.addEditor(MapEditor, { title: "Map", icon: "map" });
-    manager.addEditor(SoundEditor, { title: "Sound", icon: "sound" });
-    manager.addEditor(SpriteEditor, { title: "Sprite", icon: "sprite" });
-    return manager;
-  }, []);
-
   const theme = useTheme();
   return (
-    <EditorManagerProvider value={editorManager}> {/* will soon be removed */}
-      <StyledEngineProvider injectFirst>
-        <MUIThemeProvider theme={muiTheme}>
-          <ThemeProvider theme={theme} >
-            <BrowserRouter>
-              <Container theme={theme}>
-                <NavBar />
-                <div style={{ flex: 1, display: "flex" }}>
-                  <Routes>
-                    <Route path="/" element={<Hub />} />
-                    <Route path="/hub" element={<Hub />} />
-                    <Route path="/create" element={<GameEditor />} />
-                  </Routes>
-                </div>
-              </Container>
-            </BrowserRouter>
-          </ThemeProvider>
-        </MUIThemeProvider>
-      </StyledEngineProvider>
-    </EditorManagerProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme} >
+        <BrowserRouter>
+          <Container theme={theme}>
+            <NavBar />
+            <div style={{ flex: 1, display: "flex" }}>
+              <Routes>
+                <Route path="/" element={<Hub />} />
+                <Route path="/hub" element={<Hub />} />
+                <Route path="/create" element={<GameEditor />} />
+              </Routes>
+            </div>
+          </Container>
+        </BrowserRouter>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
---
+++ src/modules/create/game-editor/editors/SoundEditor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { EditorProps } from "./EditorType";
+
+export const SoundEditor: React.FC<EditorProps> = ({ ydoc, provider }) => {
+  return (
+    <div>
+      <h2>Sound Editor</h2>
+    </div>
+  );
+};
+
