1515a1505363ebcb6a747284e0e60b9fdb75207a
Guillame Tran <guillaume1.tran@epitech.eu>
2025-06-28 15:58:52 +0200

    [ASYNC][FEAT]: useAsync hook

--- src/modules/project/Project.tsx
+++ src/modules/project/Project.tsx
@@ -4,25 +4,26 @@ import { ProjectResponseDto, ProjectsService } from "src/api";
 import { ProjectProvider } from "src/providers/ProjectProvider";
 import GameEditor from "@modules/create/game-editor/GameEditor";
 import { Project as ProjectType } from "../../types/ProjectType";
+import { useAsync } from "src/hooks/useAsync";
 
 const Project: React.FC = () => {
   const { projectId } = useParams<{ projectId: string }>();
-  const [project, setProject] = useState<ProjectType | undefined>(undefined); //FIXME: define the type project when api ready
+  const [project, setProject] = useState<ProjectType | undefined>(undefined);
+
+  const { value } = useAsync<ProjectResponseDto | undefined>(async () => {
+    if (projectId) {
+      const project: ProjectResponseDto = await ProjectsService.projectControllerFindOne(parseInt(projectId));
+      return project;
+    }
+    return undefined;
+  }, [projectId]);
 
   useEffect(() => {
-    const fetchProject = async (): Promise<void> => {
-      if (projectId) {
-        try {
-          const project: ProjectResponseDto = await ProjectsService.projectControllerFindOne(parseInt(projectId));
-          setProject(project);
-        } catch (error) {
-          //FIXME: create a toast notification for error handling
-          console.error("Failed to fetch project:", error);
-        }
-      }
-    };
-    fetchProject();
-  }, [projectId, setProject]);
+    if (value) {
+      setProject(value);
+    }
+  }, [value]);
+
   return (
     <>
       <ProjectProvider project={project} setProject={setProject}>
