e07b972ad939b362a3ec9cbf643a4fb3ca865e4c
Louis-rollet <louis.rollet@epitech.eu>
2025-07-19 12:40:41 +0000

    [EDITOR] [FIX] react include and depricated file

--- src/modules/create/game-editor/editors/EditorType.ts
+++ src/modules/create/game-editor/editors/EditorType.ts
@@ -1,6 +1,6 @@
 import * as Y from "yjs";
 import { WebrtcProvider } from "y-webrtc";
-import React from "react";
+import { FC } from "react";
 
 export type EditorProps = {
   ydoc: Y.Doc;
@@ -11,7 +11,7 @@ export type EditorProps = {
 
 export interface EditorTab {
   label: string;
-  component: React.FC<EditorProps>;
+  component: FC<EditorProps>;
   getData?: () => string;
   setData?: (data: string) => void;
 }
