424e0ebf33e909f0c7ea09f57a7b41996948d80b
Guillame Tran <guillaume1.tran@epitech.eu>
2025-08-20 18:31:33 +0200

    [SHADER][FIX]: declare index variable in fragment shader

--- src/shared/canvas/shaders/index_to_color_frag.glsl
+++ src/shared/canvas/shaders/index_to_color_frag.glsl
@@ -7,7 +7,7 @@ uniform int u_mode;
 varying vec2 v_uv;
 
 void main() {
-    index = int(texture2D(u_texture, v_uv).r * 255.0 + 0.5);
+    int index = int(texture2D(u_texture, v_uv).r * 255.0 + 0.5);
     vec2 uv = vec2(float(index) / u_paletteSize, 0.0);
     gl_FragColor = texture2D(u_paletteTex, uv);
 }
