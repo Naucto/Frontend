285596ea8bcd9c4e8ab084aac449e42ae2d1537f
Guillame Tran <guillaume1.tran@epitech.eu>
2025-08-20 14:21:59 +0200

    [SHADER][CLEANUP]: remove unnecessary change

--- src/shared/canvas/shaders/index_to_color_frag.glsl
+++ src/shared/canvas/shaders/index_to_color_frag.glsl
@@ -7,7 +7,6 @@ uniform int u_mode;
 varying vec2 v_uv;
 
 void main() {
-    int index;
     index = int(texture2D(u_texture, v_uv).r * 255.0 + 0.5);
     vec2 uv = vec2(float(index) / u_paletteSize, 0.0);
     gl_FragColor = texture2D(u_paletteTex, uv);
