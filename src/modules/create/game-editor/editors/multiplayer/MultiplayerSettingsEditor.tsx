3f1ea97f92303a458c4e1da50c7f66f4ad157b55
Alexis Belmonte <alexbelm48@gmail.com>
2025-12-22 23:08:00 -0500

    [MULTIPLAYER] [UPDATE,NEW] Progressing on an automatically built tree

--- src/modules/create/game-editor/editors/multiplayer/MultiplayerSettingsEditor.tsx
+++ src/modules/create/game-editor/editors/multiplayer/MultiplayerSettingsEditor.tsx
@@ -1,10 +1,15 @@
 import React, { JSX, useState } from "react";
 
-import { MultiplayerDirectorySettings } from "@providers/editors/MultiplayerSettingsProvider.ts";
+import {
+  MultiplayerDirectoryFlags,
+  MultiplayerDirectorySettings,
+  MultiplayerSettingsProvider
+} from "@providers/editors/MultiplayerSettingsProvider.ts";
 import { EditorProps } from "@modules/create/game-editor/editors/EditorType";
 
 import {
   ButtonGroup,
+  Checkbox,
   IconButton,
   List,
   ListItem,
@@ -12,8 +17,50 @@ import {
   ListItemText
 } from "@mui/material";
 
+import { enumFromName } from "@our-types/enum";
+
+interface MultiplayerEntryButtonGroupProps {
+  multiplayerSettingsProvider: MultiplayerSettingsProvider,
+  path: string,
+  settings: MultiplayerDirectorySettings
+};
+
+export const MultiplayerEntryButtonGroup: React.FC<MultiplayerEntryButtonGroupProps> = ({ multiplayerSettingsProvider, path, settings }) => {
+  const mpsp = multiplayerSettingsProvider;
+
+  const flagNames = Object.keys(MultiplayerDirectoryFlags)
+    .filter(value => Number.isNaN(Number(value)) && value !== "NONE");
+
+  return (
+    <ButtonGroup size="small">
+      {/* FIXME: Use proper icons */}
+      <IconButton>Add</IconButton>
+      <IconButton>Remove</IconButton>
+      <IconButton>Rename</IconButton>
+
+      {
+        flagNames.map((flagName) => {
+          const flagValue: MultiplayerDirectoryFlags = enumFromName(MultiplayerDirectoryFlags, flagName);
+
+          const flagUpdate = (checked: boolean): void =>
+            mpsp.accessDirectorySettings(path,
+              (settings) => settings.set(flagValue, checked));
+
+          return (
+            <Checkbox
+              key={flagName}
+              checked={settings.can(flagValue)}
+              onChange={(_, checked) => flagUpdate(checked)}
+              title={flagName} />
+          );
+        })
+      }
+    </ButtonGroup>
+  );
+};
+
 export const MultiplayerSettingsEditor: React.FC<EditorProps> = ({ project }) => {
-  const createNodeEntry = (settings: MultiplayerDirectorySettings, name: string): JSX.Element => {
+  const createNodeEntry = (settings: MultiplayerDirectorySettings, path: string): JSX.Element => {
     const [isNodeOpened, setNodeOpened] = useState(false);
 
     const handleNodeToggle = (): void => {
@@ -26,29 +73,22 @@ export const MultiplayerSettingsEditor: React.FC<EditorProps> = ({ project }) =>
           disablePadding
           secondaryAction={
             <ListItemButton onClick={handleNodeToggle}>
-              <ListItemText>{name}</ListItemText>
-              <ButtonGroup size="small">
-                {/* FIXME: Use proper icons */}
-                <IconButton>Add</IconButton>
-                <IconButton>Remove</IconButton>
-                <IconButton>Rename</IconButton>
-                <IconButton>ClientAccess</IconButton>
-                <IconButton>ServerAccess</IconButton>
-              </ButtonGroup>
+              <ListItemText>{path}</ListItemText>
+              <MultiplayerEntryButtonGroup
+                multiplayerSettingsProvider={project.multiplayerSettingsProvider}
+                path={path}
+                settings={settings} />
             </ListItemButton>
           }>
           {(() => {
             if (!isNodeOpened)
               return null;
 
-            const childNodes: JSX.Element[] = [];
-
-            project.multiplayerSettingsProvider.visitDirectorySettings(
+            return project.multiplayerSettingsProvider.visitChildDirectorySettings(
+              "",
               (childSettings: MultiplayerDirectorySettings, childPath: string) =>
-                childNodes.push(createNodeEntry(childSettings, childPath))
+                createNodeEntry(childSettings, childPath)
             );
-
-            return childNodes;
           })()}
         </ListItem>
       </List>
