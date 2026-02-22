2091eb5b38de30d31c6e5de4b7c47332f7090104
Guillame Tran <guillaume1.tran@epitech.eu>
2026-02-22 16:41:20 +0900

    [CONFIG][FIX]: update BASE URL fallback to use localhost for OpenAPI

--- src/api/core/OpenAPI.ts
+++ src/api/core/OpenAPI.ts
@@ -21,7 +21,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: import.meta.env.VITE_BACKEND_URL || 'none',
+    BASE: (import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"),
     VERSION: '1.0',
     WITH_CREDENTIALS: true,
     CREDENTIALS: 'include',
