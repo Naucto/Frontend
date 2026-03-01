b9328eaca149475c74b38ab66fa1bbbe3961abb8
Guillame Tran <guillaume1.tran@epitech.eu>
2026-03-01 14:46:48 +0900

    [API][FIX] revert

--- src/api/core/OpenAPI.ts
+++ src/api/core/OpenAPI.ts
@@ -21,7 +21,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: (import.meta.env.VITE_BACKEND_URL || "none "),
+    BASE: (import.meta.env.VITE_BACKEND_URL || 'none'),
     VERSION: '1.0',
     WITH_CREDENTIALS: true,
     CREDENTIALS: 'include',
