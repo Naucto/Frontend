c6c6cf4aae1b9a679a177a72865f086a46d3dd8b
Alexis Belmonte <alexbelm48@gmail.com>
2026-03-06 10:43:16 -0500

    [CLIENT] [UPDATE] Use storage backend directly

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { UserProvider } from "@providers/UserProvider.tsx";
+import { OpenAPI } from "@api";
+import { LocalStorageManager } from "@utils/LocalStorageManager";
+
+OpenAPI.BASE = import.meta.env.VITE_BACKEND_URL ?? "";
+OpenAPI.TOKEN = () => Promise.resolve(LocalStorageManager.getToken());
 
 createRoot(document.getElementById("root")!).render(
   <UserProvider>
