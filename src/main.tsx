58298715db4d30ad966f4aee892256316e3a7791
Priax <priax.xairp@gmail.com>
2026-05-24 08:36:39 +0200

    Removed dead code

--- src/main.tsx
+++ src/main.tsx
@@ -4,23 +4,14 @@ import App from "./App.tsx";
 import { UserProvider } from "@providers/UserProvider.tsx";
 import { client } from "@api/client.gen";
 import { LocalStorageManager } from "@utils/LocalStorageManager";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./authConfig";
-
-const msalInstance = new PublicClientApplication(msalConfig);
 
 client.setConfig({
   baseUrl: import.meta.env.VITE_BACKEND_URL ?? "",
   auth: () => LocalStorageManager.getToken(),
 });
 
-msalInstance.initialize().then(() => {
-  createRoot(document.getElementById("root")!).render(
-    <MsalProvider instance={msalInstance}>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </MsalProvider>
-  );
-});
+createRoot(document.getElementById("root")!).render(
+  <UserProvider>
+    <App />
+  </UserProvider>
+);
